01c3e03c7820f62e3b025a81c94637832c2547f0
Darya Korbut <kitkatlolipop@mail.ru>
2020-10-06 20:31:16 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(element => element.join(''));
+  let i = 0;
+  while(i<bracketsConfig.length) {
+    if(str.includes(bracketsConfig[i])){
+      str = str.replace(bracketsConfig[i], '');
+      i=0;
+      continue;
+    }
+    i++;
+  }  
+  if (str.length === 0){
+      return true;
+  }else{
+      return false;
+  }
 }
