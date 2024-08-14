89d0349b452751a9367a95a86a6ff6d4da2925aa
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-08-14 22:14:04 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const mapClosedBrackets = Object.fromEntries(bracketsConfig.map(arr => [...arr].reverse()))
+  const brackets = Object.values(mapClosedBrackets)
+  const stack = []
+
+  for (let i = 0; i < str.length; i++) {
+      
+    if (brackets.includes(str[i]) && mapClosedBrackets[str[i]] === str[i] && !stack.includes(str[i])) {
+      stack.push(str[i])
+
+      continue
+    }
+
+    if (brackets.includes(str[i]) && mapClosedBrackets[str[i]] !== str[i] ) {
+      stack.push(str[i])
+
+      continue
+    }
+
+    if (stack.at(-1) !== mapClosedBrackets[str[i]]){
+
+      return false
+    }
+
+    stack.pop()
+  }
+  
+  return !stack.length
 }
